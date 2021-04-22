ac9708838070f705b6d5a8105412dabaaf371832
Mahabub Sunny <office.prosany@gmail.com>
2021-04-22 23:00:26 +0600

    Changes v1.0.5

--- src/components/Dashboard/Admin/MakeAdmin.js
+++ src/components/Dashboard/Admin/MakeAdmin.js
@@ -45,12 +45,12 @@ const MakeAdmin = () => {
             <div className="Box">
                 <form className="MakeAdminForm" onSubmit={handleSubmit(onSubmit)}>
                     <h3 style={{ color: '#6b7c93' }}>Make Admin</h3>
-                    <label>Please enter your name</label>
+                    <label>Please enter your name <span style={{color: 'red'}}>*</span></label>
                     <input type="text" {...register("name", { required: true })} />
-                    {errors.email && <span className="FormError">Email field is required</span>}
-                    <label>Please enter your email address</label>
+                    {errors.email && <span className="FormError">Name is required</span>}
+                    <label>Please enter your email address <span style={{color: 'red'}}>*</span></label>
                     <input type="email" {...register("email", { required: true })} />
-                    {errors.email && <span className="FormError">Email field is required</span>}
+                    {errors.email && <span className="FormError">Email is required</span>}
                     <button type="submit">Make Admin</button>
                 </form>
                 {makeAdmin === null ? "" : makeAdmin === true ? <p>Request to make admin is successful</p> : <p>Request to make admin is unsuccessful</p>}
